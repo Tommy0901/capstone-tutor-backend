c57db37e0527727f698acc90b8f2c0481307ba38
yuan6636 <aleniverson777@gmail.com>
2024-02-20 16:16:19 +0800

    feat: add column nation to users

---
+++ migrations/20240219073348-add-column-nation-to-users.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      ALTER TABLE Users
+      ADD COLUMN nation varchar(50) AFTER name;
+    `)
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      ALTER TABLE Users
+      DROP COLUMN nation;
+    `)
+  }
+}
