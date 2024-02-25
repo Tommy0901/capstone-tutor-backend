f02186f304cc7a702152f190dacb93e9a3deeef2
Tommy0901 <ak710180@gmail.com>
2024-02-26 05:32:42 +0800

    fear: add cors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes')
 const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json(), passport.initialize(), routes)
+app.use(cors(), express.json(), passport.initialize(), routes)
 
 app.listen(port, () => {
   console.info(`App is running on http://localhost:${port}`)
